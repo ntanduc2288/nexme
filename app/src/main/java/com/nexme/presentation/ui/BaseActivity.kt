831d106e63f5c84b313b9dc13fab2acaa558607d
Duc Nguyen <dngyuen@rei.com>
2019-07-09 09:10:25 -0700

    Added SimpleAuth library

---
+++ app/src/main/java/com/nexme/presentation/ui/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.nexme.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    abstract fun getLayoutId(): Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+    }
+}
---
+++ app/src/main/java/com/nexme/presentation/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,10 @@
+package com.nexme.presentation.ui.onboarding
+
+import com.nexme.R
+import com.nexme.presentation.ui.BaseActivity
+
+class OnboardingActivity: BaseActivity() {
+
+    override fun getLayoutId() = R.layout.onboarding_view
+
+}
